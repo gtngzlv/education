27a15262012b08e415cadc01946909abbd3aea2a
gvgatina <gvgatina@avito.ru>
2022-03-14 11:06:11 +0300

    cert added

--- Cources/Automation Selenium+Python/stepik_course/lesson_1_6_1/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture(scope="session")
-def browser():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
--- Cources/Automation Selenium+Python/stepik_course/lesson_1_6_2/stepik_pages.py
+++
@@ -1,7 +0,0 @@
-from baseapp import BasePage
-
-
-class StepikPage(BasePage):
-
-    def element_presence(self, text):
-        assert self.find_element_by_text(text) == True, "Element was not found"
